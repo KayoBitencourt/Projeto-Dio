850a9597d94b82875b0f7f44447aeb0b22ad0c88
KayoBitencourt <kayobitencourt@gmail.com>
2022-11-26 19:02:36 -0300

    AdicionandoIgualdade

--- pequenoprojeto/ProjetoDio.js
+++ pequenoprojeto/ProjetoDio.js
@@ -1,8 +1,8 @@
 /* Criando um projeto de introdução ao javascript para o primeiro repositório do github */
 const numeroperguntas = 10;
-const acertos = 2;
+const acertos = 7;
 const passounaprova = acertos * numeroperguntas;
-if(passounaprova > 70){
+if(passounaprova >= 70){
     console.log('Aprovado');
 } else {
     console.log('Reprovado');
