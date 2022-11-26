30076b866beacdc355bd149d56526a53f6b79d14
KayoBitencourt <kayobitencourt@gmail.com>
2022-11-26 18:47:43 -0300

    Adicionando o projeto

---
+++ pequenoprojeto/ProjetoDio.js
@@ -0,0 +1,9 @@
+/* Criando um projeto de introdução ao javascript para o primeiro repositório do github */
+const numeroperguntas = 10
+const acertos = 2
+const passounaprova = acertos * numeroperguntas
+if(passounaprova > 70){
+    console.log('Aprovado')
+} else {
+    console.log('Reprovado')
+}
